55f8f50758cbbca458a5f65ab3c1f0cac115b3ca
mryou04 <proriomino0039@gmail.com>
2021-04-08 01:35:22 +0900

    finish push
    finish push

---
+++ Assets/Scripts/PointCloudLoader.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+
+public class PointCloudLoader : MonoBehaviour
+{
+    [SerializeField] Shader PointCloudShader;
+    [SerializeField] TextAsset PtsFile;
+    [Range(0, 500)] public float PointRadius = 50;
+    [Range(0, 500)] public float PointSize = 100;
+
+    private ComputeBuffer posbuffer;
+    private ComputeBuffer colbuffer;
+    private Material material;
+    private List<(Vector3, Vector3)> pts;
+    private bool bufferReady = false;
+
+    async void OnEnable()
+    {
+        if (PointCloudShader == null)
+        {
+            Debug.LogError("Point Cloud Shader Not Set!");
+            return;
+        }
+
+        if (pts == null)
+        {
+            pts = await PtsReader.Load(PtsFile);
+        }
+
+        List<Vector3> positions = pts.Select(item => item.Item1).ToList();
+        List<Vector3> colors = pts.Select(item => item.Item2).ToList();
+
+        // バッファ領域を確保・セット
+        // 確保する領域サイズは、データ数 × データ一つあたりのサイズ
+        int size = Marshal.SizeOf(new Vector3());
+        posbuffer = new ComputeBuffer(positions.Count, size);
+        colbuffer = new ComputeBuffer(colors.Count, size);
+        posbuffer.SetData(positions);
+        colbuffer.SetData(colors);
+
+        // マテリアルを作成しバッファとパラメータをセット
+        material = new Material(PointCloudShader);
+        material.SetBuffer("colBuffer", colbuffer);
+        material.SetBuffer("posBuffer", posbuffer);
+        ReflectParams();
+
+        bufferReady = true;
+    }
+
+    void Update()
+    {
+        if (transform.hasChanged)
+        {
+            ReflectParams();
+        }
+    }
+
+    void OnValidate()
+    {
+        ReflectParams();
+    }
+
+    public void ReflectParams()
+    {
+        if (material != null)
+        {
+            material.SetFloat("_Radius", PointRadius);
+            material.SetFloat("_Size", PointSize);
+            material.SetVector("_WorldPos", this.transform.position);
+        }
+    }
+
+    public bool IsReady()
+    {
+        return this.bufferReady;
+    }
+
+    void OnRenderObject()
+    {
+        if (bufferReady)
+        {
+            // レンダリングのたびに頂点の個数分シェーダーを実行
+            // MeshTopology.Pointsを指定することで、面ではなく頂点が描画される            
+            material.SetPass(0);
+            Graphics.DrawProceduralNow(MeshTopology.Points, pts.Count);
+        }
+    }
+
+    void OnDisable()
+    {
+        if (bufferReady)
+        {
+            posbuffer.Release();
+            colbuffer.Release();
+        }
+        bufferReady = false;
+    }
+}
---
+++ Assets/Scripts/PtsReader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+public static class PtsReader
+{
+    async public static Task<List<(Vector3, Vector3)>> Load(TextAsset ptsfile)
+    {
+        var content = ptsfile.text;
+
+        // テキストファイルの読み込みとパースがボトルネックなのでいずれ最適化したい
+        // 現状はとりあえず非同期読み込みにしてメインスレッドがブロックされることを回避
+        return await Task.Run(() =>
+            content.Split('\n').Where(s => s != "").Select(parseRow).ToList()
+        );
+    }
+
+    private static (Vector3, Vector3) parseRow(string row)
+    {
+        var splitted = row.Split(' ').Select(float.Parse).ToList();
+
+        return (new Vector3(
+            splitted[0],
+            splitted[2], // PTSファイルは通常Z-upなので、ここでZとYを交換しY-upに変換
+            splitted[1]
+        ), new Vector3(
+            splitted[3],
+            splitted[4],
+            splitted[5]
+        ));
+    }
+}
